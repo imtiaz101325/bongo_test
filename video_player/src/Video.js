69fce2e1d12e91fddcb795786a2c06ff5be53fb3
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-08-23 23:57:29 +0600

    Added play, pause, rewind, forward button

--- video_player/src/Video.js
+++ video_player/src/Video.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useRef } from "react";
+import styled from "styled-components";
+
+const VideoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const VideoWrapper = styled.div``;
 
 export default function Video({
-  src = "",
+  src = "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4",
 }) {
+  const videoRef = useRef();
+
   return (
-    <video autoPlay>
-      <source src={src} type="video/mp4" />
-    </video>
+    <VideoContainer>
+      <video ref={videoRef}>
+        <source src={src} type="video/mp4" />
+      </video>
+      <VideoWrapper>
+        <button
+          onClick={() => {
+            videoRef.current.currentTime = videoRef.current.currentTime - 10;
+          }}
+        >
+          Rewind
+        </button>
+        <button onClick={() => videoRef.current.play()}>Play</button>
+        <button onClick={() => videoRef.current.pause()}>Pause</button>
+        <button
+          onClick={() => {
+            videoRef.current.currentTime = videoRef.current.currentTime + 10;
+          }}
+        >
+          Forward
+        </button>
+      </VideoWrapper>
+    </VideoContainer>
   );
 }
